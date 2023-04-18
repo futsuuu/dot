2a22e6163b42bd581dbba7b44e77f7c15e1b70bd
futsuuu <futsuuu123@gmail.com>
2023-04-18 22:09:50 +0900

    neovim: use prettierd instead of prettier

--- dot/neovim/lua/plugins/null-ls.lua
+++ dot/neovim/lua/plugins/null-ls.lua
@@ -5,7 +5,7 @@ local null_ls = require 'null-ls'
 null_ls.setup {
   sources = {
     null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.rustfmt,
     null_ls.builtins.formatting.black,
   },
