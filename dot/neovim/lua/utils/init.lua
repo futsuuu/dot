fe52b87d97c793c72c588a31bcf2234c17c04a36
futsuuu <futsuuu123@gmail.com>
2023-04-19 04:57:24 +0900

    neovim: add utils/path.lua

--- dot/neovim/lua/utils/init.lua
+++ dot/neovim/lua/utils/init.lua
@@ -1,26 +1,22 @@
+local Path = require 'utils.path'
+
 local M = {}
 
----Path separator
----@return string
-function M.path_sep()
-  return package.config:sub(1, 1)
-end
-
 ---List segments
----@param directory string
----@return string[]
+---@param directory Utils.Path
+---@return Utils.Path[]
 function M.ls(directory)
   local uv = vim.loop
-  local dir = uv.fs_scandir(directory)
+  local dir = uv.fs_scandir(directory:str())
   if dir == nil then
-    error('Cannot read directory `' .. directory .. '`', 2)
+    error('Cannot read directory: ' .. directory, 2)
     return {}
   end
   local items = {}
   repeat
     local name, _ = uv.fs_scandir_next(dir)
     if name then
-      table.insert(items, name)
+      table.insert(items, directory / name)
     end
   until not name
   return items
@@ -28,38 +24,31 @@ end
 
 ---Check parent directories have specific items
 ---@param items string | string[]
----@return string?
+---@return Utils.Path?
 function M.search_parent(items)
-  local function join_to_path(path)
-    local str = table.concat(path, M.path_sep())
-    if M.path_sep() == '/' then
-      str = '/' .. str
-    end
-    return str
-  end
-
   if type(items) == 'string' then
     items = { items }
   end
 
-  local path = vim.fn.expand('%:p'):split '/\\'
-  if #path == 0 then
-    return
-  end
+  local path = Path.new '%:p'
 
-  while #path > 0 do
-    table.remove(path)
-    local current_dir = join_to_path(path)
-    local list = M.ls(current_dir)
+  while path:len() > 0 do
+    path = path:parent()
+    local list = M.ls(path)
 
     for _, v in ipairs(items) do
-      ---@type string[]
-      local filtered_items = vim.tbl_filter(function(value)
-        return value == v
-      end, list)
+      ---@type Utils.Path[]
+      local filtered_items = vim.tbl_filter(
+        ---@param value Utils.Path
+        ---@return boolean
+        function(value)
+          return value:name() == v
+        end,
+        list
+      )
 
       if #filtered_items == 1 then
-        return current_dir .. M.path_sep() .. filtered_items[1]
+        return filtered_items[1]
       end
     end
   end
