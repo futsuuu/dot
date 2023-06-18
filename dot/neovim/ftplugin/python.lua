0bd076e5e612254ac5f73695e2b06f22c404eedf
futsuuu <futsuuu123@gmail.com>
2023-06-19 03:37:27 +0900

    neovim: set VIRTUAL_ENV

--- dot/neovim/ftplugin/python.lua
+++ dot/neovim/ftplugin/python.lua
@@ -1,14 +1,31 @@
 local env = vim.env
 
-if not env.VIRTUAL_ENV then
-  local venv = vim.fs.find({ 'venv', '.venv' }, {
-    upward = true,
-    stop = vim.loop.os_homedir(),
-    path = vim.fs.dirname(vim.api.nvim_buf_get_name(0)),
-    type = 'directory',
-  })
-
-  if #venv > 0 then
-    env.PATH = venv[1] .. (package.config:sub(1, 1) == '\\' and '/Scripts;' or '/bin:') .. env.PATH
-  end
+if env.VIRTUAL_ENV then
+  return
 end
+
+---@type string[]
+local venv = vim.fs.find({ 'venv', '.venv' }, {
+  upward = true,
+  stop = vim.loop.os_homedir(),
+  path = vim.fs.dirname(vim.api.nvim_buf_get_name(0)),
+  type = 'directory',
+})
+
+if #venv == 0 then
+  return
+end
+
+---@type string, string
+local virtual_env, path_sep
+
+if package.config:sub(1, 1) == '\\' then
+  virtual_env = venv[1] .. '/Scripts'
+  path_sep = ';'
+else
+  virtual_env = venv[1] .. '/bin'
+  path_sep = ':'
+end
+
+env.VIRTUAL_ENV = virtual_env
+env.PATH = virtual_env .. path_sep .. env.PATH
