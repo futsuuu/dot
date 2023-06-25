0f100875096b3e5cee9172a11e52af96b4ee0605
futsuuu <futsuuu123@gmail.com>
2023-06-25 20:51:31 +0900

    smpt: refactor

--- smpt/src/main.rs
+++ smpt/src/main.rs
@@ -1,8 +1,9 @@
-mod run;
+mod cwd;
+mod git;
+mod python;
+mod utils;
 
-use std::env;
-
-use crate::run::run;
+use std::{env, io};
 
 fn main() -> Result<(), std::io::Error> {
     let command_args = env::args().collect::<Vec<String>>();
@@ -24,5 +25,24 @@ fn main() -> Result<(), std::io::Error> {
         }
         _ => panic!("unknown command"),
     }
+    Ok(())
+}
+
+fn run(exit_status: &str, new_line: &str) -> io::Result<()> {
+    let exit_status = matches!(exit_status, "0");
+
+    let git = git::Git::new()?;
+    let python = python::Python::new();
+    let cwd = cwd::Cwd::new(git.repo_path.parent())?;
+
+    print!(
+        "{reset}{new_line}  {cwd}{git}{python}{new_line} {prompt_color}❱⟩{reset} ",
+        reset = utils::RESET,
+        prompt_color = if exit_status {
+            utils::GREEN
+        } else {
+            utils::RED
+        }
+    );
     Ok(())
 }
