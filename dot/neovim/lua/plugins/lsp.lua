d05ace0bf1b557064d857120ea02d2bff76a8b56
futsuuu <futsuuu123@gmail.com>
2023-12-26 16:36:33 +0900

    neovim: fix inlay hint

--- dot/neovim/lua/plugins/lsp.lua
+++ dot/neovim/lua/plugins/lsp.lua
@@ -14,7 +14,7 @@ require('utils').lsp.on_attach(function(client, bufnr)
 
   if client.supports_method 'textDocument/inlayHint' and vim.lsp.inlay_hint then
     vim.api.nvim_buf_create_user_command(bufnr, 'InlayHintToggle', function()
-      vim.lsp.inlay_hint(bufnr)
+      vim.lsp.inlay_hint.enable(bufnr, not vim.lsp.inlay_hint.is_enabled(bufnr))
     end, {})
   end
 end)
