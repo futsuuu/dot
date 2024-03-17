f49ed5c6f2d188cc1f78bd0004bdf65426f15b31
futsuuu <futsuuu123@gmail.com>
2024-03-18 04:08:40 +0900

    neovim(denops): update deps

--- dot/neovim/denops/deps/denops.ts
+++ dot/neovim/denops/deps/denops.ts
@@ -1,2 +1,2 @@
-export type { Denops } from "https://deno.land/x/denops_std@v6.1.0/mod.ts";
-export * as nvimFn from "https://deno.land/x/denops_std@v6.1.0/function/nvim/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v6.4.0/mod.ts";
+export * as nvimFn from "https://deno.land/x/denops_std@v6.4.0/function/nvim/mod.ts";
--- dot/neovim/denops/deps/utils.ts
+++ dot/neovim/denops/deps/utils.ts
@@ -1,2 +1,2 @@
-export { ensure, is } from "https://deno.land/x/unknownutil@v3.16.3/mod.ts";
+export { ensure, is } from "https://deno.land/x/unknownutil@v3.17.0/mod.ts";
 export { GzipStream } from "https://deno.land/x/compress@v0.4.6/gzip/mod.ts";
